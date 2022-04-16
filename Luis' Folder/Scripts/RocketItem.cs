46e7df9a7fcb58cc6873169317e63651b3d770c6
Luis A Garcia <garcia.lu@northeastern.edu>
2022-04-16 15:37:28 -0400

    Re pushed with source assets

---
+++ Luis' Folder/Scripts/RocketItem.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RocketItem : MonoBehaviour
+{
+
+    public int rotationScalar = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotationScalar, 0 * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Application.LoadLevel("Level Ver2");
+    }
+}
